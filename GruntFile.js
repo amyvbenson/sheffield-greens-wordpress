136efed0ee57fa60eac16d9ca68af804eca83f7c
Amy Benson <AmyBenson@MacBook-Pro.local>
2016-01-23 17:30:05 +0000

    Converted theme to use sass

---
+++ GruntFile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      options: {
+          sourceMap: true
+      },
+      dist: {
+          files: {
+              'style.css': 'styles/styles.scss'
+          }
+      }
+    },
+
+    watch: {
+      sass: {
+        files: 'styles/*.scss',
+        tasks: ['sass'],
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['sass']);
+
+};
