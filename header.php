9429885db199a184fe03ce50196b8ea386e3db2e
Amy Benson <avbenson@gmail.com>
2016-02-23 23:38:24 +0000

    fixed title on homepage

--- header.php
+++ header.php
@@ -26,19 +26,19 @@
     <![endif]-->
     
     <title>
-    <?php if ( is_home() ) { ?><?php bloginfo('name'); ?>&nbsp;|&nbsp;<?php bloginfo('description'); ?><?php } ?>
+    <?php if ( is_front_page() ) { ?><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?><?php } ?>
     
-    <?php if ( is_search() ) { ?><?php bloginfo('name'); ?>&nbsp;|&nbsp;Search Results<?php } ?>
+    <?php if ( is_search() ) { ?><?php bloginfo('name'); ?> | Search Results<?php } ?>
     
-    <?php if ( is_single() ) { ?><?php wp_title(''); ?>&nbsp;|&nbsp;<?php bloginfo('name'); ?><?php } ?>
+    <?php if ( is_single() ) { ?><?php wp_title(''); ?> | <?php bloginfo('name'); ?><?php } ?>
     
-    <?php if ( is_page() ) { ?><?php bloginfo('name'); ?>&nbsp;|&nbsp;<?php wp_title(''); ?><?php } ?>
+    <?php if ( is_page() && !is_front_page() ) { ?><?php bloginfo('name'); ?> | <?php wp_title(''); ?><?php } ?>
     
-    <?php if ( is_category() ) { ?><?php bloginfo('name'); ?>&nbsp;|&nbsp;Archive&nbsp;|&nbsp;<?php single_cat_title(); ?><?php } ?>
+    <?php if ( is_category() ) { ?><?php bloginfo('name'); ?> | Archive | <?php single_cat_title(); ?><?php } ?>
     
-    <?php if ( is_month() ) { ?><?php bloginfo('name'); ?>&nbsp;|&nbsp;Archive&nbsp;|&nbsp;<?php the_time('F'); ?><?php } ?>
+    <?php if ( is_month() ) { ?><?php bloginfo('name'); ?> | Archive | <?php the_time('F'); ?><?php } ?>
     
-    <?php if (function_exists('is_tag')) { if ( is_tag() ) { ?><?php bloginfo('name'); ?>&nbsp;|&nbsp;Tag Archive&nbsp;|&nbsp;<?php  single_tag_title("", true); } } ?>
+    <?php if (function_exists('is_tag')) { if ( is_tag() ) { ?><?php bloginfo('name'); ?> | Tag Archive | <?php  single_tag_title("", true); } } ?>
     </title>
     
     <?php
@@ -62,8 +62,6 @@
 </head>
 
 <body <?php body_class($class); ?>>
-
-
 <!-- BEGIN OPTIONS POPUP -->
 <div id="utility" class="top-header">
 <?php include(TEMPLATEPATH . '/includes/popup-panel.php'); ?>
