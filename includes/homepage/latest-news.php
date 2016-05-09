af890e6250063265e6f3bd53d04ca177381181a2
Amy Benson <avbenson@gmail.com>
2016-05-09 22:38:25 +0100

    only show featured posts on homepage

--- includes/homepage/latest-news.php
+++ includes/homepage/latest-news.php
@@ -2,6 +2,8 @@
   <h2 class="home-section__heading">Latest News</h2>
 
   <?php 
+  $featuredcat = get_option('green_featured_category'); // ID of the Featured Category
+  $ex_feat = $wpdb->get_var("SELECT term_id FROM $wpdb->terms WHERE name='$featuredcat'");
   $the_query = new WP_Query('cat=' . $ex_feat . '&showposts=5&orderby=post_date&order=desc');
   while ($the_query->have_posts()) : $the_query->the_post(); $do_not_duplicate = $post->ID;
   ?>
