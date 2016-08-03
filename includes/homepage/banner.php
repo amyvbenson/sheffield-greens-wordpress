f7ed5abe5ab0ef7b568efe8bf234525ff64a2a54
Amy Benson <avbenson@gmail.com>
2016-08-03 20:21:50 +0100

    conference banner

--- includes/homepage/banner.php
+++ includes/homepage/banner.php
@@ -1,11 +1,14 @@
 <div class="container container--no-pad home-banner home-banner--new home-banner--full-image">
 
-  <a href="https://my.greenparty.org.uk/civicrm/membership/joining" class="home-banner__link">
-    
-    <!-- <img src="<?php bloginfo('template_url'); ?>/images/join-banner.jpg" alt=" " /> -->
-    <!-- <span class="sr-only">Join the Green Party Now. Together we must now mobilise to defend our hard-won rights and environmental protections, and stand up for migrants.</span>
-     -->
-    <div class="home-banner__content"><h2 class="home-banner__heading">Together we must now mobilise to defend our hard-won rights and environmental protections, and stand up for migrants.<br><strong>Join the Green Party now to be part of the fight</strong></h2></div>
+  <a href="https://www.greenparty.org.uk/conference/" class="home-banner__link">
+
+    <div class="home-banner__content">
+      <h2 class="home-banner__heading">
+        <strong>Autumn Conference 2016</strong>
+        2-4 September, University of Birmingham
+        <strong>Book your place now</strong>
+        </h2>
+    </div>
 
   </a>
   
