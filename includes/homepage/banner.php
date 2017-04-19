b1e494f600b0b1287f03f513a836a6f745cb2a64
Amy Benson <avbenson@gmail.com>
2017-04-19 11:48:54 +0100

    Update home banner link

--- includes/homepage/banner.php
+++ includes/homepage/banner.php
@@ -1,6 +1,6 @@
 <div class="container container--no-pad home-banner home-banner--new home-banner--full-image">
 
-  <a href="http://sheffieldgreenparty.org.uk/about-us/donate/" class="home-banner__link">
+  <a href="http://sheffieldgreenparty.org.uk/teamnatalie/" class="home-banner__link">
 
     <div class="home-banner__content">
       <h2>A Green voice in the North</h2>
