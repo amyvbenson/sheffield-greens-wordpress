573dc5b27b0e3c86292841d11d49dc27bbbc4d15
Amy Benson <avbenson@gmail.com>
2017-04-19 11:20:36 +0100

    Election 2017 templates

---
+++ page-sub-election-2017.php
@@ -0,0 +1,56 @@
+<?php /* Template Name: 2017 Election Campaign sub page */ ?>
+
+<?php  include(TEMPLATEPATH . '/includes/header/head.php');  ?>
+
+<body <?php body_class($class); ?>>
+
+  <?php  include(TEMPLATEPATH . '/includes/header/header.php');  ?>
+
+  <div class="container container--no-pad campaign--nb">
+
+      <article class="page-section page-section--large post">
+      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+        <div class="col-70">
+          <div class="campaign__sub-page-heading">
+            <h1 >
+              <?php the_title(); ?>
+            </h1>
+          </div>
+
+          <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+
+            <div class="post__body">
+
+              <?php the_content(); ?>
+
+            </div>
+
+        </div>
+        </div>
+        <div class="col-30 campaign__sidebar">
+          <a href="http://sheffieldgreenparty.org.uk/about-us/donate/" class="campaign__sidebar-cta-block">
+            <span>Donate now to help Greens win in Sheffield</span>
+          </a>
+
+          <a href="http://sheffieldgreenparty.org.uk/get-involved/teamnatalie-signup" class="campaign__sidebar-cta-block">
+            <span>Get involved and help Natalie win</span>
+          </a>
+
+          <a href="https://www.gov.uk/register-to-vote" class="campaign__sidebar-cta-block">
+            <span>Register to vote</span>
+          </a>
+
+
+      </div>
+      <?php endwhile; ?>
+      </article>
+
+
+    </div>
+
+  </div>
+  <?php get_footer(); ?>
+</body>
+</html>
