ddb5206e8903c23d9e4e9340587be2051395b4ff
Amy Benson <avbenson@gmail.com>
2017-05-04 19:37:51 +0100

    Updated elections sidebar

--- includes/elections_sidebar.php
+++ includes/elections_sidebar.php
@@ -3,26 +3,17 @@
   <h3>Register to vote now!</h3>
 
   <p><a href="https://www.gov.uk/register-to-vote">Register to vote online</a>.</p>
-  <p>Register by 7 June to vote in the EU referendum on 23 June.</p>
-  
+  <p>Register by 22 May to vote in the General Election on 8 June.</p>
+
 
   <h3>Are you likely to be out of Sheffield on polling day?</h3>
 
-  <p>Don't worry you can still vote, find out how to apply for a postal or proxy vote:</p>
-
-  <p>Postal vote: <a href="http://www.sheffield.gov.uk/your-city-council/elections/voting-by-post" target="_new" >http://www.sheffield.gov.uk/your-city-council/elections/voting-by-post</a>.</p>
-
-  <p>Proxy vote: <a href="http://www.sheffield.gov.uk/your-city-council/elections/voting-by-proxy" target="_new" >http://www.sheffield.gov.uk/your-city-council/elections/voting-by-proxy</a></p>
+  <p>Don't worry you can still vote, <a href="https://www.gov.uk/voting-in-the-uk/postal-voting">find out how to apply for a postal or proxy vote</a>.</p>
 
   <h3>Why not give us a hand?</h3>
 
-  <p>We are always keen to hear from people willing to lend a hand with   delivering leaflets, canvassing for votes, or spending some time with   activities. <a href="/2016/04/04/help-sheffield-greens-win-in-2016/">Find out ways you can get involved here</a>.</p>
+  <p>We are always keen to hear from people willing to lend a hand with   delivering leaflets, canvassing for votes, or spending some time with   activities. <a href="./teamnatalie">Find out ways you can get involved here</a>.</p>
 
-  <h3>Election Links</h3>
-
-  <p><a href="http://yorkshireandhumber.greenparty.org.uk/region/yorkshireandhumber" target="_self"><strong>Yorkshire and Humber Green Party</strong></a>              </p>
-  <p><a href="http://www.greenparty.org.uk/policies.html"><strong>National Policies: </strong>Key priorities for Green Party Action</a></p>
-  <p><a href="http://www.youtube.com/greenpartyew"><strong>YouTube: </strong>Visit the Green Party's YouTube channel</a></p>
 
   <h3>Download a window poster</h3>
 
