b9a771b0c1593a996f352d2d39c8e5c626485338
Amy Benson <avbenson@gmail.com>
2017-05-04 22:15:37 +0100

    Updated election banner on homepage

--- includes/homepage/banner.php
+++ includes/homepage/banner.php
@@ -1,14 +1,10 @@
 <div class="container container--no-pad home-banner home-banner--new home-banner--full-image">
-
+<h2>
   <a href="http://sheffieldgreenparty.org.uk/teamnatalie/" class="home-banner__link">
-
-    <div class="home-banner__content">
-      <h2>A Green voice in the North</h2>
-      <p>Help us elect Natalie Bennett<br> in Sheffield Central</p>
-    </div>
-
+    <span class="sr-only">Vote Green on June 8th - Natalie Bennett, Sheffield Central</span>
   </a>
-
+</h2>
+  <a href="http://sheffieldgreenparty.org.uk/elections/general-elections/general-election-2017/" class="home-banner__secondary">
+  <span class="sr-only">Declan Walsh, Sheffield Heeley. Christine Gilligna Kubo, Brightside and Hillsborough. Logan Robin, Sheffield Hallam</span>
   </a>
-
 </div>
