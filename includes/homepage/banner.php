458a0df98fe2f32747deac1eb06957a6db9f7969
Amy Benson <avbenson@gmail.com>
2017-06-23 09:42:08 +0100

    updated homepage banner

--- includes/homepage/banner.php
+++ includes/homepage/banner.php
@@ -1,11 +1,11 @@
 <div class="container container--no-pad home-banner home-banner--nes">
  <h2>
-<div>
+<a href="/elections/council-elections/rob-unwin-nes/">
   <span class="col-30">
     <strong>Thank you</strong> to all Green voters in Nether Edge &amp; Sharrow
   </span>
   <span class="col-70">
   </span>
-</div>
+</a>
 </h2>
 </div>
