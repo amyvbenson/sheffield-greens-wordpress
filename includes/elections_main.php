ff4ea28eb06cfaf82d642a22967fe14f48ee472c
Amy Benson <avbenson@gmail.com>
2018-01-05 14:22:50 +0000

    Removed parish elections from elections page

--- includes/elections_main.php
+++ includes/elections_main.php
@@ -6,7 +6,7 @@
 
     <div class="elections-section council">
 
-      <div class="election-summary">            
+      <div class="election-summary">
         <a rel="nofollow">
           <img src="<?php bloginfo('template_url'); ?>/thumb.php?src=/images/elections/townhall.jpg&amp;h=230&amp;w=230&amp;zc=1" alt="Link to <?php the_title(); ?>" class="thumbnail"/>
         </a>
@@ -25,7 +25,7 @@
 
     <div class="elections-section parliamentary">
 
-      <div class="election-summary">            
+      <div class="election-summary">
         <a rel="nofollow"><img src="<?php bloginfo('template_url'); ?>/thumb.php?src=/images/elections/bigben.jpg&amp;h=230&amp;w=230&amp;zc=1" alt="Link to <?php the_title(); ?>" class="thumbnail"/>
         </a>
         <p>Last election Date: <span class="election-date"><?php if ( get_option('green_last_general_election') <> "" ) { echo stripslashes(get_option('green_last_general_election')); } ?></span></p>
@@ -43,7 +43,7 @@
 
     <div class="elections-section european">
 
-      <div class="election-summary">            
+      <div class="election-summary">
         <a rel="nofollow"><img src="<?php bloginfo('template_url'); ?>/thumb.php?src=/images/elections/europeanparliament.jpg&amp;h=230&amp;w=230&amp;zc=1" alt="Link to <?php the_title(); ?>" class="thumbnail"/>
         </a>
         <p>Last election Date: <span class="election-date"><?php if ( get_option('green_last_european_election') <> "" ) { echo stripslashes(get_option('green_last_european_election')); } ?></span></p>
@@ -54,25 +54,7 @@
       <div class="election-info">
         <h1>European Elections</h1>
         <?php if ( get_option('green_info_european_election') <> "" ) { echo stripslashes(get_option('green_info_european_election')); } ?>
-        <a rel="nofollow" class="readmore" href="<?php bloginfo('url'); ?>/elections/european-elections">More on European Elections &raquo;</a>              
-      </div>
-
-    </div>
-
-    <div class="elections-section parish">
-
-      <div class="election-summary">            
-        <a rel="nofollow"><img src="<?php bloginfo('template_url'); ?>/thumb.php?src=/images/elections/sheffieldpc.jpg&amp;h=230&amp;w=230&amp;zc=1" alt="Link to <?php the_title(); ?>" class="thumbnail"/>
-        </a>
-        <p>Last election Date: <span class="election-date"><?php if ( get_option('green_last_parish_election') <> "" ) { echo stripslashes(get_option('green_last_parish_election')); } ?></span></p>
-        <p>Next election Date: <span class="election-date"><?php if ( get_option('green_next_parish_election') <> "" ) { echo stripslashes(get_option('green_next_parish_election')); } ?></span></p>
-        <p>For more information, past election results and candidates <a href="<?php bloginfo('url'); ?>/elections/parish-elections">click here</a></p>
-      </div>
-
-      <div class="election-info">
-        <h1>Parish Elections</h1>
-        <?php if ( get_option('green_info_parish_election') <> "" ) { echo stripslashes(get_option('green_info_parish_election')); } ?>              
-        <a rel="nofollow" class="readmore" href="<?php bloginfo('url'); ?>/elections/parish-elections">More on Parish Elections &raquo;</a>              
+        <a rel="nofollow" class="readmore" href="<?php bloginfo('url'); ?>/elections/european-elections">More on European Elections &raquo;</a>
       </div>
 
     </div>
